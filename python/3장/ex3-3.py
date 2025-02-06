c017e9bec73675d989480bd3938bc4e15d09e12f
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-06 11:45:48 +0900

    if, elif, else

---
+++ python/3장/ex3-3.py
@@ -0,0 +1,14 @@
+score = int(input("점수는?"))
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else :
+    grade = "D"
+
+print("등급:", grade)
