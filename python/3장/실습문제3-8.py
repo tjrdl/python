b6d10602fdad6beb28a7f12fc348663e2e195f13
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-06 15:01:04 +0900

    if, else, lower

---
+++ python/3장/실습문제3-8.py
@@ -0,0 +1,18 @@
+str = input("'사자'의 영어 단어는 무엇일까요? ").lower()
+if str == "lion":
+    print("딩동댕! 참 잘했어요!")
+else:
+    print("땡! 틀렸습니다.")
+
+str2 = input("'오렌지'의 영어 단어는 무엇일까요? ").lower()
+if str2 == "orange":
+    print("딩동댕! 참 잘했어요!")
+else:
+    print("땡! 틀렸습니다.")
+
+
+str3 = input("'기차'의 영어 단어는 무엇일까요? ").lower()
+if str3 == "train":
+    print("딩동댕! 참 잘했어요!")
+else:
+    print("땡! 틀렸습니다.")
