cbbd4c67fb3b4dbc5851758e769d2b509c2d9455
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-06 10:50:42 +0900

    들여쓰기 수정

--- python/실습문제2-12.py
+++ python/실습문제2-12.py
@@ -2,10 +2,10 @@ price = int(input("책 값은?"))
 dec = int(input("할인율은?"))
 ship = int(input("배송료는?"))
 
-result = price - (price*(dec/100)) + ship
+result = price - (price * (dec / 100)) + ship
 
-print("책 값:",price,"원")
-print("할인율:",dec)
-print("배송료:",ship,"원")
+print("책 값:", price, "원")
+print("할인율:", dec)
+print("배송료:", ship, "원")
 
-print("결제 금액: ",int(result),"원")
+print("결제 금액: ", int(result), "원")
