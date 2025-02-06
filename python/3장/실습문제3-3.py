865614e8d082b718ca1b857f38e25cae3cb9efaf
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-06 12:48:55 +0900

    if, elif 문자열 비교

---
+++ python/3장/실습문제3-3.py
@@ -0,0 +1,20 @@
+grade = input("등급을 입력해 주세요: ")
+
+if grade == "A+":
+    print("등급:", grade, "평점:4.5")
+elif grade == "A":
+    print("등급:", grade, "평점:4.0")
+elif grade == "B+":
+    print("등급:", grade, "평점:3.5")
+elif grade == "B":
+    print("등급:", grade, "평점:3.0")
+elif grade == "C+":
+    print("등급:", grade, "평점:2.5")
+elif grade == "C":
+    print("등급:", grade, "평점:2.0")
+elif grade == "D+":
+    print("등급:", grade, "평점:1.5")
+elif grade == "D":
+    print("등급:", grade, "평점:1.0")
+elif grade == "F":
+    print("등급:", grade, "평점:0.0")
