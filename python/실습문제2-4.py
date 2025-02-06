6550f19381e6470a3c6db7c1ec30becf80ee8527
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-06 09:28:13 +0900

    input(), print(), 콤마(,)

---
+++ python/실습문제2-4.py
@@ -0,0 +1,5 @@
+fruit1 = input("첫번째 과일을 입력하세요 :")
+fruit2 = input("두번째 과일을 입력하세요 :")
+
+print("%s 와(과) %s 은(는) 내가 좋아하는 과일이다." % (fruit1, fruit2))
+print(fruit1, "와(과)", fruit2, "은(는) 내가 좋아하는 과일이다.")
