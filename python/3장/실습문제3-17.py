29f74f040d6299435bd7e7e2503d4d83ad84b8f6
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-06 16:51:21 +0900

    계산기 들여쓰기 수정

--- python/3장/실습문제3-17.py
+++ python/3장/실습문제3-17.py
@@ -7,16 +7,13 @@ num1 = int(input("첫 번째 숫자를 입력하세요"))
 num2 = int(input("두 번째 숫자를 입력하세요"))
 
 
-if str == "1" :
-    print(num1,"+",num2,"=",num1+num2)
-elif str == "2" :
-    print(num1,"-",num2,"=",num1-num2)
-elif str == "3" :
-    print(num1,"*",num2,"=",num1*num2)
-elif str == "4" :
-    print(num1,"/",num2,"=%.2f"%(num1/num2))
-else :
+if str == "1":
+    print(num1, "+", num2, "=", num1 + num2)
+elif str == "2":
+    print(num1, "-", num2, "=", num1 - num2)
+elif str == "3":
+    print(num1, "*", num2, "=", num1 * num2)
+elif str == "4":
+    print(num1, "/", num2, "=%.2f" % (num1 / num2))
+else:
     print("선택 오류!")
-
-
-
