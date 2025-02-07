ee98bc93165f218edd76fa21003a900e0bb3a34b
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-07 16:18:14 +0900

    list

---
+++ 5장/ex5-1.py
@@ -0,0 +1,9 @@
+list1 = [3.15, -12.5, "apple", "starwberry"]
+
+print(list1)
+
+
+# list2 = list(range(10))
+list2 = list(range(1, 10)) # 1 ~ 9
+list2 = list(range(1, 21, 2))  # 1에서 2씩 증가한 값
+print(list2)
