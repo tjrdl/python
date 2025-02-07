d33b453393e959ec1be7c702b5462e2103a51b9a
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-07 17:41:38 +0900

    이차원 list

---
+++ 5장/ex5-27.py
@@ -0,0 +1,11 @@
+number = [[10,20,30],[40,50,60,70,80]]
+
+print(number[0][0])
+print(number[0][1])
+print(number[0][2])
+print(number[1][0])
+print(number[1][1])
+print(number[1][2])
+print(number[1][3])
+print(number[1][4])
+    
