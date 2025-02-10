8af6b66728ed6f23c8905ff2a7a20df89fef4229
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-10 11:39:29 +0900

    dict, key, value

---
+++ python/6장/ex6-5.py
@@ -0,0 +1,8 @@
+area_code = {"서울": "02", "부산": "051", "대구": "053", "광주": "062"}
+print(area_code)
+
+for key in area_code:
+    # key
+    print(key, end=" ")
+    # value
+    print(area_code[key])
