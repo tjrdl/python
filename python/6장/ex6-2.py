14303c3ae3c32ff54e9547d2e70cdb1ee4344be5
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-10 11:38:48 +0900

    dictionary

---
+++ python/6장/ex6-2.py
@@ -0,0 +1,16 @@
+# tup1 = (10,20,30,40,50)
+# tup1 = (10,20)
+# tup2 = (30,40)
+# tup3 = tup1 + tup2
+
+# for i in range(len(tup1)):
+#     print(tup1[i])
+
+# print(tup3)
+
+admin_info = ("admin", "12345", "webmaster@naver.com")
+
+print("관리자 정보")
+print("아이디: " + admin_info[0])
+print("비밀번호: " + admin_info[1])
+print("이메일: " + admin_info[2])
