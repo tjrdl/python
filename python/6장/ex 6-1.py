0d2837bf94358645be30bc70978fa609e32c66b4
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-10 11:38:40 +0900

    tuple, list

---
+++ python/6장/ex 6-1.py
@@ -0,0 +1,12 @@
+animals = ["토끼", "거북이", "사자", "여우"]
+
+
+n = tuple(range(10, 21))
+print(n)
+
+print("n[0]=", n[0])
+print("n[2:5]=", n[2:5])
+print("n[2:]=", n[2:])
+print("n[:5]=", n[:5])
+print("n[-2]=", n[-2])
+print("n[::-1]=", n[::-1])
