74a61a68eeff27454b1ecbe36b11885f9e2364b8
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-10 15:20:58 +0900

    함수선언, 매개변수

---
+++ python/7장/ex7-1.py
@@ -0,0 +1,12 @@
+# 함수: 없음, 반환값: 없음
+# def hello():
+#     print("hello")
+
+
+#  매개변수를를 가짐
+def say_hello(f_name, s_name):
+    name = f_name + s_name
+    print(name, "님 안녕하세요")
+
+
+say_hello("J", "JY")
