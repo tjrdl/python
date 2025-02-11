c63ab9c7b7811195fa0781caf7e7759013ec5151
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-11 12:51:11 +0900

    import math

---
+++ python/9장/9-3.py
@@ -0,0 +1,14 @@
+import math
+
+print("sin(2):",math.sin(2))
+print("cos(2):",math.cos(2))
+print("tan(2):",math.tan(2))
+print("ceil(12.3):",math.ceil(12.3))
+print("floor(12.3):",math.floor(12.3))
+print("round(12.3):",round(12.3))
+print(math.fsum([1,2,3,4,5]))
+print(math.fsum([1.7,2.3,3.2,4,5]))
+print(math.fsum((1.7,2.3,3.2,4,5)))
+print("log(12.3):",math.log(12.3))
+print("pow(12,3):",math.pow(12,3))
+print("sqrt(25):",math.sqrt(25))
